08d7e5c69ffc54e99bdaa77e646ddd74720da9e1
Sam Malcolm <samuel.malcolm@rmit.edu.au>
2017-09-30 20:35:52 +1000

    Finished grand final day work, still initial builds

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-function getBallCount() {
+var urlParams = new URLSearchParams(window.location.search);
 
-    var url = window.location.href;
-    var queryIndex = url.indexOf('reds=');
-    var count = url.substr(queryIndex+5,queryIndex+6);
+var reds = urlParams.get('reds');
 
-    document.querySelector("title").innerHTML = count+" Ball Game";
+console.log(reds);
+var player1=true;
 
-    return count;
+function changePlayer() {
+        console.log("Burtton CLicked Player 1");
+        document.querySelector("#player1").classList.toggle("current_player");
+        document.querySelector("#player2").classList.toggle("current_player");
+    document.querySelector("#player1log").innerHTML += "<div class=\"ball_log\" id=\"end\"></div>";
+        player1=false;
+
+
 }
 
-var reds = getBallCount();
+function redPot() {
+    document.querySelector("#player1log").innerHTML += "<div class=\"ball_log\" id=\"red\"></div>";
+}
+
+function blackPot() {
+    document.querySelector("#player1log").innerHTML += "<div class=\"ball_log\" id=\"black\"></div>";
+}
+
+function pinkPot() {
+    document.querySelector("#player1log").innerHTML += "<div class=\"ball_log\" id=\"pink\"></div>";
+}
+
+function bluePot() {
+    document.querySelector("#player1log").innerHTML += "<div class=\"ball_log\" id=\"blue\"></div>";
+}
+
+document.querySelector("#endbreak").addEventListener("click", changePlayer, false);
+
+document.querySelector(".ball#red").addEventListener("click", redPot, false);
+document.querySelector(".ball#pink").addEventListener("click", pinkPot, false);
+document.querySelector(".ball#black").addEventListener("click", blackPot, false);
+document.querySelector(".ball#blue").addEventListener("click", bluePot, false);
