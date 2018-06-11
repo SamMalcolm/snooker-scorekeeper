9cd76d1a86be69c233383b456976927ef43ba5bc
Sam Malcolm <samuel.malcolm@rmit.edu.au>
2018-06-11 11:13:14 +1000

    undo working a lot more elegant than previous iteration

--- ui-redesign/script.js
+++ ui-redesign/script.js
@@ -9,6 +9,7 @@ var redoLog = [],
     snookersRequired = 0,
     framesPlayed = 0,
     player1break,
+    freeBallActive = false,
     p1name,
     p2name,
     p1hc,
@@ -17,7 +18,8 @@ var redoLog = [],
     logBottomLayer = document.querySelector(".log-bottom-layer"),
     divsInTopLog = document.querySelectorAll(".log-top-layer div"),
     divsInBottomLog = document.querySelectorAll(".log-bottom-layer div"),
-    urlParams = new URLSearchParams(window.location.search);
+    urlParams = new URLSearchParams(window.location.search),
+    balls = document.querySelectorAll(".ball");
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -30,6 +32,8 @@ function init() {
         p1hc = urlParams.get("p1hc");
         p2hc = urlParams.get("p2hc");
     }
+
+    //add frame count if multiple frames palyed
 
 
     if (!framesPlayed) {
@@ -50,18 +54,125 @@ function init() {
             player1break = true;
         }
     }
+
+    for (let i=0;i<balls.length;i++) {
+        balls[i].classList.add("disabled");
+    }
+
+    document.querySelector(".redmenu").classList.remove("disabled");
     console.log(player1active);
     populateUI();
 }
 
 function loopThroughLog() {
     //calculate scores during loop and also populate log then populate ui with scores
+    //reset values to zero?
+    player1score = 0;
+    player2score = 0;
+    remaining = 0;
+    reds=15;
+    for (let i = 0; i < log.length; i++) {
+        // Balls Potted
+        if (log[i].indexOf("RED") !== -1) {
+            reds--;
+            remaining = (8*reds + 34);
+            if (log[i].substr(0,1) == "1") {
+                player1score++;
+            } else {
+                player2score++;
+            }
+        }
+        if (log[i].indexOf("PINK") !== -1) {
+            remaining = (8*reds + 27);
+            if (log[i].substr(0,1) == "1") {
+                player1score = player1score+6;
+            } else {
+                player2score = player2score+6;
+            }
+        }
+        if (log[i].indexOf("BLACK") !== -1) {
+            remaining = (8*reds + 27);
+            if (log[i].substr(0,1) == "1") {
+                player1score = player1score+7;
+            } else {
+                player2score = player2score+7;
+            }
+        }
+        if (log[i].indexOf("BLUE") !== -1) {
+            remaining = (8*reds + 27);
+            if (log[i].substr(0,1) == "1") {
+                player1score = player1score+5;
+            } else {
+                player2score = player2score+5;
+            }
+        }
+        if (log[i].indexOf("BROWN") !== -1) {
+            remaining = (8*reds + 27);
+            if (log[i].substr(0,1) == "1") {
+                player1score = player1score+4;
+            } else {
+                player2score = player2score+4;
+            }
+        }
+        if (log[i].indexOf("GREEN") !== -1) {
+            remaining = (8*reds + 27);
+            if (log[i].substr(0,1) == "1") {
+                player1score = player1score+3;
+            } else {
+                player2score = player2score+3;
+            }
+        }
+        if (log[i].indexOf("YELLOW") !== -1) {
+            remaining = (8*reds + 27);
+            if (log[i].substr(0,1) == "1") {
+                player1score = player1score+2;
+            } else {
+                player2score = player2score+2;
+            }
+        }
+        //BALLS POTTED END
+        // FOULS
 
+        //FOULS END
+
+
+
+
+
+
+
+
+    }
+
+    console.log(log);
 
     populateUI();
 }
 
 function populateUI() {
+
+    if (log.length > 0) {
+    if (log[log.length - 1].indexOf("RED") == -1) {
+
+        for (let i=0;i<balls.length;i++) {
+            balls[i].classList.add("disabled");
+        }
+        document.querySelector(".redmenu").classList.remove("disabled");
+
+    } else {
+        for (let i=0;i<balls.length;i++) {
+            if (balls[i].classList.contains("disabled")) {
+                balls[i].classList.remove("disabled");
+            }
+        }
+        document.querySelector(".redmenu").classList.add("disabled");
+    }
+    } else {
+        for (let i=0;i<balls.length;i++) {
+            balls[i].classList.add("disabled");
+        }
+        document.querySelector(".redmenu").classList.remove("disabled");
+    }
     //check active player and adjust accordingly
 
     //populate scores
@@ -78,6 +189,7 @@ function populateUI() {
 
 function addToLog(logItem) {
     log.push(logItem);
+    loopThroughLog();
 }
 
 function undo() {
@@ -87,3 +199,54 @@ function undo() {
 }
 
 init();
+
+document.querySelector(".redmenu").addEventListener("click",function() {
+    if (player1active) {
+        addToLog("0_RED");
+    } else {
+        addToLog("1_RED");
+    }
+});
+document.querySelector(".pinkborder").addEventListener("click",function() {
+    if (player1active) {
+        addToLog("0_PINK");
+    } else {
+        addToLog("1_PINK");
+    }
+});
+document.querySelector(".blackborder").addEventListener("click",function() {
+    if (player1active) {
+        addToLog("0_BLACK");
+    } else {
+        addToLog("1_BLACK");
+    }
+});
+document.querySelector(".blueborder").addEventListener("click",function() {
+    if (player1active) {
+        addToLog("0_BLUE");
+    } else {
+        addToLog("1_BLUE");
+    }
+});
+document.querySelector(".brownborder").addEventListener("click",function() {
+    if (player1active) {
+        addToLog("0_BROWN");
+    } else {
+        addToLog("1_BROWN");
+    }
+});
+document.querySelector(".greenborder").addEventListener("click",function() {
+    if (player1active) {
+        addToLog("0_GREEN");
+    } else {
+        addToLog("1_GREEN");
+    }
+});
+document.querySelector(".yellowborder").addEventListener("click",function() {
+    if (player1active) {
+        addToLog("0_YELLOW");
+    } else {
+        addToLog("1_YELLOW");
+    }
+});
+document.querySelector(".undo").addEventListener("click",undo,false);
